27d12c3c64f2b187dbfbef5155a7706990077575
amchampoux <amchampoux@mirego.com>
2022-10-24 00:08:49 -0400

    add eqArrays.js file

---
+++ eqArrays.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  } 
+
+};
+
+const eqArrays = function(array1, array2) {
+
+  for (i = 0; i< array1.length; i++) {
+    for (j = 0; i<array2.length; j++) {
+      // console.log(i);
+      if (array1[i] === array2[j]) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+}
+
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS

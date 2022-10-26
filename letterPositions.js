f915843f09ee29d3475ec8a3f5ae344e99ba3c91
amchampoux <amchampoux@mirego.com>
2022-10-26 00:02:15 -0400

    add letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,41 @@
+const eqArrays = function(array1, array2) {
+  for (i = 0; i< array1.length; i++) {
+    for (j = 0; j<array2.length; j++) {
+      if (array1[i] === array2[j]) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+}
+
+const assertArraysEqual = function(array1, array2) {
+    if (eqArrays(array1, array2) === true) {
+      return console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+    } else {
+      return console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+    }  
+  }
+
+const letterPositions = function(sentence) {
+  
+  let results = {};
+
+  for (i = 0; i < sentence.length; i++) {
+
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      results[sentence[i]] = [i];
+    }
+  }
+
+  return results;
+  
+};
+
+console.log(letterPositions('hello'));
+assertArraysEqual(letterPositions("hello").h, [0]);
+assertArraysEqual(letterPositions("hello").e, [1]);
+assertArraysEqual(letterPositions("hello").l, [2, 3]);

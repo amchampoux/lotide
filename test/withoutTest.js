bb2abe76876a3cf50a36007d1dd567245d36904d
amchampoux <amchampoux@mirego.com>
2022-11-13 22:20:13 -0500

    refactor without.js function

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -7,6 +7,5 @@ assertArraysEqual(without([1, 2, 3, 4, 5, 6], [2, 6]), [1, 3, 4, 5]);
 assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]);
 
 const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
+without(words, ["lighthouse"]);
 assertArraysEqual(words, ["hello", "world", "lighthouse"]);
--- without.js
+++ without.js
@@ -1,20 +1,8 @@
 const without = function(array, itemsToRemove) {
 
-  let output = [];
+  array = array.filter((item) => !itemsToRemove.includes(item));
+  return array;
 
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < itemsToRemove.length; j++) {
-
-      if (array[i] === itemsToRemove[j]) {
-        i++;
-      }
-      if (i === array.length) {
-        return output;
-      }
-    }
-    output.push(array[i]);
-  }
-  return output;
 };
 
 module.exports = without;
